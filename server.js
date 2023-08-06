e3797d01e7f6a2b4d3e14cd8e557cb0a943d2faf
Darshan Gowda <iamdarshangowda@gmail.com>
2023-08-06 18:11:01 +0530

    domain dot added

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(
     store: sessionStore,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
-      domain: 'todo-app-fullstack-git-module-oauth-iamdarshangowda.vercel.app',
+      domain: '.vercel.app',
     }, // 1days
   })
 );
