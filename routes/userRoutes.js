bd6a9ab5f00639dac5eb56393f820a87c5e264db
Darshan Gowda <iamdarshangowda@gmail.com>
2023-10-07 18:13:58 +0530

    added verify route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { userSignup, userLogin, userRedirect } = require('../controllers/userController');
+const verifyToken = require('../middlewares/verifyToken');
 
 router.post('/signup', userSignup);
 
 router.post('/login', userLogin);
+
+router.get('/verify', verifyToken);
 
 module.exports = router;
